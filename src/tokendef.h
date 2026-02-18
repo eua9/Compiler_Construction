e5713e047432b09b495849e77553409cad5bf7dd
PrinceyPie <rymitchell9416@gmail.com>
2026-02-18 15:19:53 -0600

    Starting FIles: ready for testing.

---
+++ src/tokendef.h
@@ -0,0 +1,57 @@
+/* major stuff */
+#define ID         251
+#define INTCONST   252
+#define CHARCONST  253
+#define STRCONST   254
+
+/* keywords */
+#define KWD_IF     255
+#define KWD_ELSE   256
+#define KWD_WHILE  257
+#define KWD_INT    258
+#define KWD_STRING 259
+#define KWD_CHAR   260
+#define KWD_RETURN 261
+#define KWD_VOID   262
+
+/* operators */
+#define OPER_ADD    263
+#define OPER_SUB    264
+#define OPER_MUL    265
+#define OPER_DIV    266
+#define OPER_LT     267
+#define OPER_GT     268
+#define OPER_GTE    269
+#define OPER_LTE    270
+#define OPER_EQ     271
+#define OPER_NEQ    272
+#define OPER_ASGN    273
+
+/* brackets & parens */
+#define LSQ_BRKT    274
+#define RSQ_BRKT    275
+#define LCRLY_BRKT  276
+#define RCRLY_BRKT  277
+#define LPAREN     278
+#define RPAREN     279
+
+/* punctuation */
+#define COMMA      280
+#define SEMICLN    281
+
+/* other */
+#define OPER_NEG   282
+#define OPER_MOD   283
+#define OPER_AT    284
+#define OPER_INC   285
+#define OPER_DEC   286
+#define OPER_AND   287
+#define OPER_OR    288
+#define OPER_NOT   289
+#define ERROR      290
+#define ILLEGAL_TOK 291
+#define COMMENT    292
+#define NEWLINE    293
+#define WHITESPACE  294
+#define MULTLNCOMMENT 295
+
